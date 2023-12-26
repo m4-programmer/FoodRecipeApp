b113f3173758baf4e956e439405c93357b4abb0f
Adolphus Miracle <miraboy13@gmail.com>
2023-12-26 23:15:09 +0100

    feat: added nativewind, tailwind and react navigation

--- App.js
+++ App.js
@@ -3,18 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View className="flex-1 items-center justify-center bg-red-500 text-white">
+    <Text className="text-white">Open up App.js to start working on your app!</Text>
+    <StatusBar style="light" />
+  </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
