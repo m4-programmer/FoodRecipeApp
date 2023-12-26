860586a712a12f26ad5388fa3093765302e6044d
Adolphus Miracle <miraboy13@gmail.com>
2023-12-26 23:19:57 +0100

    feat: added heroicons library and tested it

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { SparklesIcon } from "react-native-heroicons/outline";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-red-500 text-white">
     <Text className="text-white">Open up App.js to start working on your app!</Text>
+    <SparklesIcon />
     <StatusBar style="light" />
   </View>
   );
