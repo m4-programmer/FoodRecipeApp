f9cf4f2e472bee672f7920504af42b19e0ea70d2
Adolphus Miracle <miraboy13@gmail.com>
2023-12-27 15:41:52 +0100

    feat: implemented the welcome escreen

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,14 +3,16 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import HomeScreen from '../screens/HomeScreen';
+import WelcomeScreen from '../screens/WelcomeScreen';
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home' screenOptions={{headerShown: false}}>
+      <Stack.Navigator initialRouteName='Welcome' screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
